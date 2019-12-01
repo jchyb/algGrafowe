a5ef70c1474e1119220637c0b971a498debbf75d
Jan <jan.chyb.99@gmail.com>
2019-12-02 00:05:15 +0100

    added ford fulkenson

--- lab2popr.py
+++ lab2popr.py
@@ -1,9 +1,9 @@
 from dimacs import *
 from collections import deque
 
-(V, L) = loadDirectedWeightedGraph("graphs-lab3/clique20")
+(V, L) = loadDirectedWeightedGraph("graphs-lab2/flow/grid5x5")
 
-def bfs(v,rGraph,wMat):
+def bfs(v,V,rGraph,wMat):
     d = deque()
     visited = [False for i in range(V)]
     parent = [-1 for i in range(V)]
@@ -11,39 +11,43 @@ def bfs(v,rGraph,wMat):
     while len(d) != 0:
         el = d.popleft()
         visited[el] = True
+        #print(" ",el)
         for i in range(len(rGraph[el])):
+            #print("  ",rGraph[el][i])
             if not visited[rGraph[el][i]] and wMat[(el,rGraph[el][i])][0] - wMat[(el,rGraph[el][i])][1] > 0:
+                #print("   ", rGraph[el][i])
                 visited[rGraph[el][i]] = True
                 d.append(rGraph[el][i])
                 parent[rGraph[el][i]]= el
     return parent
 
-def fordFulkenson(L, u, v):
+def fordFulkenson(L,V, u, v):
     rGraph = [[] for i in range(V)]
     wMat = {}
     for l in L:
         rGraph[l[0] - 1].append(l[1] - 1)
         rGraph[l[1] - 1].append(l[0] - 1)
-
         wMat[(l[0] - 1, l[1] - 1)] = [l[2], 0]
         wMat[(l[1] - 1, l[0] - 1)] = [0, 0]
 
-
     fMax=0
     while True:
-        parent = bfs(u,rGraph, wMat)
+        parent = bfs(u, V, rGraph, wMat)
 
+        #for p in parent:
+         #   print(p)
         if parent[v]==-1: break
 
         i = v
         par = parent[i]
         fDelta = wMat[(par,i)][0] - wMat[(par,i)][1]
-        while (i != 0):
+        #print("fdel",fDelta)
+        while (i != u):
             par = parent[i]
             fDelta = min(fDelta, wMat[(par,i)][0] - wMat[(par,i)][1])
             i = par
-        i = V-1
-        while i != 0:
+        i = v
+        while i != u:
             par = parent[i]
             wMat[(par,i)][1] += fDelta
             wMat[(i,par)][1] -= fDelta
@@ -52,9 +56,12 @@ def fordFulkenson(L, u, v):
         fMax += fDelta
     return fMax
 
+print(fordFulkenson(L,V,0,V-1))
+
+'''
 minCut = 1e14
-for i in range(len(L)):
-    for j in range(len(L)):
-        if i != j:
-            minCut = min(minCut, fordFulkenson(L,0,V-1))
+for i in range(1,V):
+    minCut = min(minCut, fordFulkenson(L,V,0,i))
+    print(minCut)
 print(minCut)
+'''
